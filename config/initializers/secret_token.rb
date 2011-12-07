016b21d8ea7043adbc0aed22df5af73e6fc09e43
Stephen Cavaliere <steve@delasteve.com>
2011-12-07 03:08:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sherlock::Application.config.secret_token = '37f68e7ffeafa705eca79442b9d3a9a7ab43e191a5619b05e5f50579a1d1007ecd93815295e1adcee377cc61ceded323f112711034da96f4489004377ac0b83e'
